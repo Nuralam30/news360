c4fa5c07244ac198704afb91899e7f5488fae3c7
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-05-03 15:10:04 +0600

    banner section done

---
+++ client/news.js
@@ -0,0 +1,92 @@
+
+export const news = [
+    {
+        country: "Afganisthan",
+        headline: "Nothing we can do but watch babies die",
+        subHeadline: "Despiration in Afganistan hospitals",
+        title: "The grim hospital where even basic needs cannot be met and children die of preventable diseases.",
+        category: "news",
+        by: {
+                name: "Yogita Limaye",
+                profession: "Afghanistan correspondent"
+            },
+        images: [
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/859F/production/_129570243_taybullah.jpg.webp",
+                imgHeadline: "Tayabullah breathes oxygen through a tube held by his mother"
+            },
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/2784/production/_129561101_motherward.png.webp",
+                imgHeadline: "Mothers are left distraught at this Afghanistan hospital where children die of preventable or curable diseases"
+            }
+        ],
+        description: "Three-month-old Tayabullah is quiet and motionless. His mother Nigar moves the oxygen pipe away from his nose and puts a finger below his nostrils to check if she can feel him breathing. She begins to cry as she realises her son is fading. At this hospital in Afghanistan, there is not a single working ventilator. Mothers hold oxygen tubes near their babies' noses because masks designed to fit their small faces are not available, and the women are trying to fill in for what trained staff or medical equipment should do. Every day, 167 children die in Afghanistan from preventable diseases, according to the UN children's fund Unicef - illnesses that could and should be cured with the right medication. It is a staggering number. But it's an estimate. And when you step inside the paediatric ward of the main hospital in the western province of Ghor, you will be left wondering if that estimate is too low. Multiple rooms are full of sick children, at least two in each bed, their little bodies ravaged by pneumonia. Just two nurses look after 60 children. In one room, we saw at least two dozen babies who appeared to be in a serious condition. The children should have been continuously monitored in critical care - impossible at this hospital. Yet, for the million people who live in Ghor, this basic facility is still the best equipped public hospital they can access."
+    },
+
+    {
+        country: "Afganisthan",
+        headline: "Nothing we can do but watch babies die",
+        subHeadline: "Despiration in Afganistan hospitals",
+        title: "The grim hospital where even basic needs cannot be met and children die of preventable diseases.",
+        category: "news",
+        by: {
+                name: "Yogita Limaye",
+                profession: "Afghanistan correspondent"
+            },
+        images: [
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/859F/production/_129570243_taybullah.jpg.webp",
+                imgHeadline: "Tayabullah breathes oxygen through a tube held by his mother"
+            },
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/2784/production/_129561101_motherward.png.webp",
+                imgHeadline: "Mothers are left distraught at this Afghanistan hospital where children die of preventable or curable diseases"
+            }
+        ],
+        description: "Three-month-old Tayabullah is quiet and motionless. His mother Nigar moves the oxygen pipe away from his nose and puts a finger below his nostrils to check if she can feel him breathing. She begins to cry as she realises her son is fading. At this hospital in Afghanistan, there is not a single working ventilator. Mothers hold oxygen tubes near their babies' noses because masks designed to fit their small faces are not available, and the women are trying to fill in for what trained staff or medical equipment should do. Every day, 167 children die in Afghanistan from preventable diseases, according to the UN children's fund Unicef - illnesses that could and should be cured with the right medication. It is a staggering number. But it's an estimate. And when you step inside the paediatric ward of the main hospital in the western province of Ghor, you will be left wondering if that estimate is too low. Multiple rooms are full of sick children, at least two in each bed, their little bodies ravaged by pneumonia. Just two nurses look after 60 children. In one room, we saw at least two dozen babies who appeared to be in a serious condition. The children should have been continuously monitored in critical care - impossible at this hospital. Yet, for the million people who live in Ghor, this basic facility is still the best equipped public hospital they can access."
+    },
+    {
+        country: "Afganisthan",
+        headline: "Nothing we can do but watch babies die",
+        subHeadline: "Despiration in Afganistan hospitals",
+        title: "The grim hospital where even basic needs cannot be met and children die of preventable diseases.",
+        category: "news",
+        by: {
+                name: "Yogita Limaye",
+                profession: "Afghanistan correspondent"
+            },
+        images: [
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/859F/production/_129570243_taybullah.jpg.webp",
+                imgHeadline: "Tayabullah breathes oxygen through a tube held by his mother"
+            },
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/2784/production/_129561101_motherward.png.webp",
+                imgHeadline: "Mothers are left distraught at this Afghanistan hospital where children die of preventable or curable diseases"
+            }
+        ],
+        description: "Three-month-old Tayabullah is quiet and motionless. His mother Nigar moves the oxygen pipe away from his nose and puts a finger below his nostrils to check if she can feel him breathing. She begins to cry as she realises her son is fading. At this hospital in Afghanistan, there is not a single working ventilator. Mothers hold oxygen tubes near their babies' noses because masks designed to fit their small faces are not available, and the women are trying to fill in for what trained staff or medical equipment should do. Every day, 167 children die in Afghanistan from preventable diseases, according to the UN children's fund Unicef - illnesses that could and should be cured with the right medication. It is a staggering number. But it's an estimate. And when you step inside the paediatric ward of the main hospital in the western province of Ghor, you will be left wondering if that estimate is too low. Multiple rooms are full of sick children, at least two in each bed, their little bodies ravaged by pneumonia. Just two nurses look after 60 children. In one room, we saw at least two dozen babies who appeared to be in a serious condition. The children should have been continuously monitored in critical care - impossible at this hospital. Yet, for the million people who live in Ghor, this basic facility is still the best equipped public hospital they can access."
+    },
+    {
+        country: "Afganisthan",
+        headline: "Nothing we can do but watch babies die",
+        subHeadline: "Despiration in Afganistan hospitals",
+        title: "The grim hospital where even basic needs cannot be met and children die of preventable diseases.",
+        category: "news",
+        by: {
+                name: "Yogita Limaye",
+                profession: "Afghanistan correspondent"
+            },
+        images: [
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/859F/production/_129570243_taybullah.jpg.webp",
+                imgHeadline: "Tayabullah breathes oxygen through a tube held by his mother"
+            },
+            {
+                image: "https://ichef.bbci.co.uk/news/976/cpsprodpb/2784/production/_129561101_motherward.png.webp",
+                imgHeadline: "Mothers are left distraught at this Afghanistan hospital where children die of preventable or curable diseases"
+            }
+        ],
+        description: "Three-month-old Tayabullah is quiet and motionless. His mother Nigar moves the oxygen pipe away from his nose and puts a finger below his nostrils to check if she can feel him breathing. She begins to cry as she realises her son is fading. At this hospital in Afghanistan, there is not a single working ventilator. Mothers hold oxygen tubes near their babies' noses because masks designed to fit their small faces are not available, and the women are trying to fill in for what trained staff or medical equipment should do. Every day, 167 children die in Afghanistan from preventable diseases, according to the UN children's fund Unicef - illnesses that could and should be cured with the right medication. It is a staggering number. But it's an estimate. And when you step inside the paediatric ward of the main hospital in the western province of Ghor, you will be left wondering if that estimate is too low. Multiple rooms are full of sick children, at least two in each bed, their little bodies ravaged by pneumonia. Just two nurses look after 60 children. In one room, we saw at least two dozen babies who appeared to be in a serious condition. The children should have been continuously monitored in critical care - impossible at this hospital. Yet, for the million people who live in Ghor, this basic facility is still the best equipped public hospital they can access."
+    }
+]
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,71 @@
 
+import { news } from './../news';
+
 const Home = () => {
     return (
-        <>
-            <h1>Welcome to home page</h1>
-        </>
+        <div className="homepage">
+            <div className="adsense">
+                <h6>here places the google adsense 1</h6>
+            </div>
+
+            <div className="container-fluid home-content">
+                {/* ========= banner section here ========= */}
+                <div className="row banner-content">
+                    <div className="banner-heading">
+                        <h4>Welcome to news360.com</h4>
+                        <p className='font-14 gray'>Wednesday, 3 May</p>
+                    </div>
+                    <div className="col-md-6 mb-2">
+                        <div className="top-news" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(${news[0].images[0].image})` }}>
+                            <div className="news-content">
+                                <h2>{news[0].headline}</h2>
+                                <p className="font-9">{news[0].title}</p>
+                                <span className="left-border font-8">{news[0].country}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="row">
+                            {
+                                news.map(n => <div className="col-md-6">
+                                    <div className="update-news" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(${n.images[0].image})` }}>
+                                        <div className="news-content">
+                                            <h5>{n.headline}</h5>
+                                            <span className="left-border font-8">{n.country}</span>
+                                        </div>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row body-content">
+                    <p className="left-border mb-3 ms-3">News</p>
+                    <div className="col-md-8">
+                        <div className="row">
+                            {
+                                news.map(ne => <div className='col-md-4 mb-3'>
+                                    <div className="newses">
+                                        <img className='mb-2' src={ne.images[0].image} alt="new headline picture" />
+                                        <h5>{ne.headline}</h5>
+                                        <p className='font-9 gray'>{ne.title}</p>
+                                        <span className='left-border font-8'>{ne.country}</span>
+                                    </div>
+                                </div>)
+                            }
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="adsense h-100">
+                            this is adsense 2
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
